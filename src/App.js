1507e44c23d37461b1714987a29fa3651bd08368
kartik-desai <desaikartik99@gmail.com>
2019-09-18 13:48:42 +0530

    First

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import About from './components/About';
+function App() {
+  return (
+    <div className="App">
+      <About></About>
+    </div>
+  );
+}
+
+export default App;
